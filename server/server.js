a07662ff0e6d79362ed3bb70f127be5e18a9121e
ChrisSensebe <christophe.sensebe@gmail.com>
2016-10-23 22:11:58 +0200

    creating express server listening on port 3000 in server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 /**
  * Created by nizural on 16/10/16.
  */
-// importing library
+// imports
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var SerialPort = require('serialport');
 
 // creating new serialPort
 var serialPort = new SerialPort('/dev/ttyACM0', {
     baudRate: 9600,
     parser: SerialPort.parsers.readline('\n')
+});
+
+app.get('/', function (req, res) {
+    res.send('<h1>Garden Bot</h1>');
 });
 
 // responding to events
@@ -17,4 +24,8 @@ serialPort.on('open', function () {
        console.log(Date.now());
        console.log(data);
    });
+});
+
+http.listen(3000, function () {
+    console.log('Garden bot server listening on port 3000');
 });
